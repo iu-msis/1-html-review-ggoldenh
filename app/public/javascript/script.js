d426e03c5e51957ea42caea22a733c4d725a6efb
ggoldenh <88209592+ggoldenh@users.noreply.github.com>
2021-09-21 16:36:05 -0400

    updating javascript

--- app/public/javascript/script.js
+++ app/public/javascript/script.js
@@ -1,41 +1,36 @@
-const url =' https://randomuser.me/api/ ';
-
-let avatar = document.getElementById('avatar');
-let fullname = document.getElementById('fullname');
-let username = document.getElementById('username');
-let email = document.getElementById('email');
-let city = document.getElementById('city');
-let btn = document.getElementById('btn');
-
-btn.addEventListener("click", function() {
-  fetch(url)
-    .then(handleErrors)
-    .then(parseJSON)
-    .then(updateProfile)
-    .catch(printError)
-});
-
-function handleErrors (res){
-  if(!res.ok){
-    throw error(res.status);
-  }
-  console.log(res);
-  return res;
+const SomeApp = {
+  data() {
+    return {
+      result: {},
+    }
+  },
+  computed: {
+      prettyBirthday() {
+          return dayjs(this.person.dob.date)
+          .format('D MMM YYYY');
+      }
+  },
+  methods: {
+    fetchUserData() {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((json) => {
+            console.log("Got json back:", json);
+            this.result = json.results[0];
+            console.log("C");
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
+    }
+},
+created() {
+    this.fetchUserData();
 }
 
-function parseJSON (res){
-  return res.json();
 }
 
-function updateProfile (profile){
-  avatar.src = profile.results[0].picture.medium;
-  fullname.innerHTML = profile.results[0].name.first +" "+profile.results[0].name.last; 
-  username.innerHTML = profile.results[0].login.username; 
-  email.innerHTML = profile.results[0].email;
-  city.innerHTML = profile.results[0].location.city;
-  return 1;
-}
+Vue.createApp(SomeApp).mount('#someApp');
 
-function printError (error){
-  console.log(error);
-}
+
+
